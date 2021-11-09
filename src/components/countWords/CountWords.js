490b617dc3698f7d01e5d86ecf0160a85c943947
Andrey Larikov <larikov174@gmail.com>
2021-11-10 01:30:21 +0300

    fix: get words count

--- src/components/countWords/CountWords.js
+++ src/components/countWords/CountWords.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { PopupWithNote } from "../popupWithNote/PopupWithNote";
 
-export const CountWords = ({ textBlock, selectionIndex, isVisible }) => {
-
-  const [modalVisibleState, setModalVisibleState] = useState(isVisible);
-  const modalClose = () => setModalVisibleState(false);
-  const index = selectionIndex;
-  const text = textBlock;
+export const CountWords = () => {
+  const getData = useSelector((state) => state.counter);
+  const index = getData.index;
+  const text = getData.text;
   const cut = text.slice(0, index).replace(/\.?,?:?-?;?/g, "");
   const regExp = /([а-я]+|[0-9]+|[a-z]+)\s/gi;
-
+  const [popupOpened, setPopupOpened] = useState(false);
+  const modalClose = () => setPopupOpened(false);
+  console.log(`${cut.match(regExp).length + 1}`);
 
   return (
-    <PopupWithNote isOpen={modalVisibleState} isCLosed={modalClose}>
-      <h2 className="popup__title">
-        `Вы успели прочитать: ${cut.match(regExp).length + 1} слов.`
-      </h2>
+    <PopupWithNote isOpen={popupOpened} isCLosed={modalClose}>
+      <h2 className="popup__title">`Вы успели прочитать: ${cut.match(regExp).length + 1} слов.`</h2>
     </PopupWithNote>
   );
 };
